9ebe6d075351e3e07e00c944aa683a093efd799f
neisenmenger <neisenmenger@gmail.com>
2012-11-13 16:21:15 -0800

    preliminary code to read in a binary file. it should work on a little endian machine, but something appears to be wrong with the output. It reads in the binary data file into a char array, and then casts it as unsigned shorts. Iam not sure where it is going wrong. It currently prints a tab delimited text file of the data for comparison purposes.

---
+++ ReadBinFile.cpp
@@ -0,0 +1,64 @@
+//============================================================================
+// Name        : pointere_example.cpp
+// Author      : Nancy Eisenmenger
+//============================================================================
+
+#include <iostream>
+#include <fstream>
+#include <string.h>
+using namespace std;
+
+
+int main()
+{
+
+	unsigned short* datamatrix;
+	int row = 2157;
+	int column = 2112;	
+	int j;	
+	ifstream::pos_type size;
+	char * memblock;
+	cout << "test" << endl;
+	ifstream datafile;
+//	datafile.open ("~/../../../cygdrive/c/Users/Nancy/Documents/Cpp_Project/data/cl_bpan15_t60_12x3_01_avg_05060546.bin", ios::in | ios::binary | ios::ate ); 
+	datafile.open ("./cl_bpan15_t60_12x3_01_avg_05060546.bin", ios::in|ios::binary|ios::ate);
+	if (datafile.is_open())
+	 { 
+	size = datafile.tellg();
+	memblock = new char [size];
+	datafile.seekg (0, ios::beg);
+	datafile.read (memblock, size);// assumes that the machine reads little endian
+	datafile.close();
+	cout << "length of char array is " << size << endl;
+	cout << "the complete file content is in memory" << endl;
+	datamatrix = new (nothrow) unsigned short [size/2];
+	if(datamatrix)
+	{
+		memcpy(datamatrix, memblock, size);	
+		cout << "Data is "; 
+/*	for (int i = 0; i < size/2; i++)
+		{	
+			cout << datamatrix[i] << " ";		
+		}*/ 
+	}		
+	delete[] memblock;
+}
+  else cout << "Unable to open file";	
+
+	ofstream outfile;
+	outfile.open("testout.csv", ios::out);
+	for(j = 0; j < size/2; j++)
+	{
+		outfile << datamatrix[j] ;
+		if(j > 1 && (j+1)%column == 0)
+		{
+		outfile << endl;
+	//	cout << j << " ";
+		}
+		else
+		outfile << "	";	
+	}
+	outfile.close();	
+	return 0;
+}
+
